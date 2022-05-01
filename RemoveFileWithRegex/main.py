db406f3ef828106e467ef3b7cb499d0641fe898b
aniloli42 <aniloli42@gmail.com>
2022-05-01 09:33:49 +0545

    Updated Version: can delete nested files

--- RemoveFileWithRegex/main.py
+++ RemoveFileWithRegex/main.py
@@ -1,18 +1,43 @@
 import os
 import re
 
-# Replace the PASTE_HERE_YOUR_FILE_PATH with your file path
-FILE_PATH = "PASTE_HERE_YOUR_FILE_PATH"
+# Get the Inputs from the User
+FILE_PATH = input("Enter the File Path: ")
+FILE_PATTERN = input("Enter the File Pattern: ")
+PATTERN_FILE_STATUS = int(input("You want to Keep (0) or Remove(1) Pattern File: "))
 
-fileList = os.listdir(FILE_PATH)
+def RemoveFileWithPattern(PATH):
+    # Get the list of Files from provided path
+    listDir = os.listdir(PATH)
 
-for file in fileList:
-    fileName = file
 
-    # main line which helps to match the file
-    isMatched = re.search("Copy", fileName)
+    # Loop through the list
+    for listFile in listDir:
+        # Check the file or directory
+        isFile = re.search("\.[a-zA-Z0-9]+$", listFile)
 
-    # if match with our codition then
-    if isMatched:
-        # delete the file through this line
-        os.remove(FILE_PATH + "/" + fileName)
+
+        # when list item is directory
+        if not isFile:
+
+            # Goes to inner directory
+            RemoveFileWithPattern(f"{PATH}/{listFile}")
+
+            # skip the current loop
+            continue
+
+        # Match the file with user pattern
+        isPatternedFile = bool(re.search(FILE_PATTERN, listFile))
+
+        """
+        Cases:
+            Case 1: File Matched & Keep == true (0)
+                Remove the matched files only
+
+            Case 2: File Matched & Keep == false (1)
+                Remove the non match files only
+        """
+        if isPatternedFile == PATTERN_FILE_STATUS:
+            os.remove(f"{PATH}/{listFile}")
+
+RemoveFileWithPattern(FILE_PATH)
